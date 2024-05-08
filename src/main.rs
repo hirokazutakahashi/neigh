6195f49541b548df0207c7628cce0cb38d659f8a
Hirokazu Takahashi <hirokazu@takaha.sh>
2024-05-08 21:36:22 +0900

    Implement command line argument parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+//! Simple IPv6/v4 neighbor discovery tool.
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(about, long_about = None)]
+struct Args {
+    /// Network interface name
+    #[arg(short, long)]
+    interface: String,
+
+    /// Host name or IPv6/IPv4 address
+    host: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args);
 }
